dbe73c8d9142e91f091d3b2f43741829ee1e2bf1
HenriqueCruz22 <henriquedacruz@ienh.com.br>
2023-06-19 20:46:46 -0300

    API

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ document.getElementById("button-Translate").addEventListener("click", function()
 
   // Enviar o texto para o servidor Flask
   fetch("/translate", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: "text=" + encodeURIComponent(textToTranslate)
+      method: "POST",
+      headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: "text=" + encodeURIComponent(textToTranslate)
   })
   .then(function(response) {
-    return response.text();
+      return response.text();
   })
   .then(function(data) {
-    // Exibir a tradução no campo de texto
-    document.getElementById("text-area-To").value = data;
+      // Exibir a tradução no campo de texto
+      document.getElementById("text-area-To").value = data;
   })
   .catch(function(error) {
-    console.log("Ocorreu um erro:", error);
+      console.log("Ocorreu um erro:", error);
   });
 });
