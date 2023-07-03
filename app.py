d1aba2fe784495cd4ac43a59a13763f8598eb248
HenriqueCruz22 <henriquedacruz@ienh.com.br>
2023-07-03 19:21:14 -0300

    a

--- app.py
+++ app.py
@@ -4,46 +4,33 @@ app = Flask(__name__)
 
 translations = {
     "hello": {
-        "en": "Hello",
-        "pt": "Olá",
-        "es": "Hola"
+        "pt": "olá",
+        "es": "hola",
+        "fr": "bonjour"
     },
     "cat": {
-        "en": "cat",
         "pt": "gato",
-        "es": "gato"
+        "es": "gato",
+        "fr": "chat"
     },
-    # Adicione mais palavras e traduções aqui
+    "dog": {
+        "pt": "cão",
+        "es": "perro",
+        "fr": "chien"
+    }
 }
 
-@app.route('/translate', methods=['GET', 'POST'])
+@app.route('/translate', methods=['POST'])
 def translate():
-    if request.method == 'POST':
-        data = request.get_json()
-        word = data.get('word')
-        lang = data.get('lang')
+    data = request.get_json()
+    word = data.get('word')
+    lang = data.get('lang')
 
-        if word is None or lang is None:
-            return jsonify({'error': 'Palavra e idioma são campos obrigatórios.'}), 400
-
-        translation = translations.get(word.lower())
-
-        if translation is None:
-            return jsonify({'error': f'Tradução não encontrada para a palavra "{word}".'}), 404
-
-        translated_word = translation.get(lang.lower())
-
-        if translated_word is None:
-            return jsonify({'error': f'Idioma "{lang}" não encontrado.'}), 404
-
-        return jsonify({'word': translated_word})
-    
-    elif request.method == 'GET':
-        return jsonify({'message': 'Para traduzir, envie uma requisição POST para esta rota com a palavra e o idioma desejado.'})
-
-@app.route('/')
-def home():
-    return jsonify({'message': 'Bem-vindo à API de tradução. Acesse /translate para usar o serviço.'})
+    if word in translations and lang in translations[word]:
+        translated_word = translations[word][lang]
+        return jsonify({"translated_word": translated_word})
+    else:
+        return jsonify({"error": "Tradução não encontrada para a palavra fornecida."})
 
 if __name__ == '__main__':
     app.run(debug=True)
