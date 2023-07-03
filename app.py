f5a3757c75c7e07d7d37eb2064ab43c8b6acdb33
HenriqueCruz22 <henriquedacruz@ienh.com.br>
2023-07-03 19:25:55 -0300

    a

--- app.py
+++ app.py
@@ -3,34 +3,44 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 translations = {
-    "hello": {
-        "pt": "olá",
-        "es": "hola",
-        "fr": "bonjour"
+    'hello': {
+        'pt': 'olá',
+        'es': 'hola',
+        'fr': 'bonjour'
     },
-    "cat": {
-        "pt": "gato",
-        "es": "gato",
-        "fr": "chat"
+    'cat': {
+        'pt': 'gato',
+        'es': 'gato',
+        'fr': 'chat'
     },
-    "dog": {
-        "pt": "cão",
-        "es": "perro",
-        "fr": "chien"
+    'dog': {
+        'pt': 'cachorro',
+        'es': 'perro',
+        'fr': 'chien'
     }
 }
 
 @app.route('/translate', methods=['POST'])
 def translate():
     data = request.get_json()
-    word = data.get('word')
-    lang = data.get('lang')
 
-    if word in translations and lang in translations[word]:
-        translated_word = translations[word][lang]
-        return jsonify({"translated_word": translated_word})
-    else:
-        return jsonify({"error": "Tradução não encontrada para a palavra fornecida."})
+    word = data.get('word')
+    language = data.get('language')
+
+    if not word or not language:
+        return jsonify({'error': 'Por favor, forneça uma palavra e um idioma para tradução.'}), 400
+
+    translation = translations.get(word.lower())
+
+    if not translation:
+        return jsonify({'error': 'Tradução não encontrada para a palavra fornecida.'}), 404
+
+    translation_text = translation.get(language.lower())
+
+    if not translation_text:
+        return jsonify({'error': 'Tradução não encontrada para o idioma fornecido.'}), 404
+
+    return jsonify({'word': word, 'translation': translation_text})
 
 if __name__ == '__main__':
     app.run(debug=True)
